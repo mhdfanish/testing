332177b7b188e6fadce738a8d692c0d73c6729d1
mhdfanish <fanishnasi@gmail.com>
2024-03-11 12:33:24 +0530

    update

--- pythonProject2/markTest.py
+++ pythonProject2/markTest.py
@@ -3,13 +3,13 @@ from grade import mark
 
 class testmar(unittest.TestCase):
     def testAdd(self):
-        self.assertTrue(mark,msg="A")
+        self.assertTrue(90,msg="A")
     def testAdd(self):
-        self.assertTrue(mark,msg="B")
+        self.assertTrue(80,msg="B")
     def testAdd(self):
-        self.assertTrue(mark,msg="C")
+        self.assertTrue(70,msg="C")
     def testAdd(self):
-        self.assertTrue(mark,msg="D")
+        self.assertTrue(60,msg="D")
 
 if __name__=='__main__':
     unittest.main()
